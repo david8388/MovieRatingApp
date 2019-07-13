e17bfe736ea4e16f6ff7648290cd09249f6fa0d2
David <hsienwu830808@gmail.com>
2019-07-13 10:51:11 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+// remember to npm install sass-loader node-sass --save-dev to prevent error
+// https://webpack.docschina.org/loaders/sass-loader/
+import '@/assets/stylesheets/main.scss'
+
+export default {
+  name: 'App'
+}
+</script>
